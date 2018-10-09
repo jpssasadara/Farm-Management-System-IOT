0f009bbaf222c7c22c3aa0d308619e7aa5888fdc
j.p.s sasadara <jpssasadara1995@gmail.com>
2018-10-09 17:16:00 +0530

    configfile

---
+++ iot part tem and hum/api/weather/dbconfig.php
@@ -0,0 +1,9 @@
+<?php
+
+
+define('DB_USER', "id6815778_sasadaramonker");     
+define('DB_PASSWORD', "sasadaramonker");			
+define('DB_DATABASE', "id6815778_sasadaramonker"); 
+define('DB_SERVER', "localhost");			
+
+?>
