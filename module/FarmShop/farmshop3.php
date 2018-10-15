f00ddd9ba2c6107cb3dfb4750a426a1a20248280
Hasee Amarathunga <haseeamarathunga@gmail.com>
2018-10-15 13:35:51 -0700

    Update Farmshop reports

--- module/FarmShop/farmshop3.php
+++ module/FarmShop/farmshop3.php
@@ -1,17 +1,15 @@
 <?php
-function getReport($itemName,$date){ 
+function getReport($itemName){ 
     $con = mysqli_connect("localhost", "root", "", "fmsmy");  
-    $sql = "SELECT tbl_order_item.item_name,sum(tbl_order_item.order_item_quantity) AS quantity,
-    sum(tbl_order_item.order_item_price) AS price,sum(tbl_order_item.order_item_actual_amount) AS total
-    FROM tbl_order_item  
-    LEFT JOIN tbl_order ON tbl_order_item.order_id=tbl_order.order_id WHERE tbl_order_item.item_name='$itemName' and tbl_order.order_date LIKE '%$date%'";
+    $sql = "SELECT item_name,sum(order_item_quantity) AS quantity,sum(order_item_price) AS price,sum(order_item_actual_amount) AS total
+    FROM tbl_order_item  WHERE item_name='$itemName';";
 
 
     $result = mysqli_query($con,$sql);
     $response = array();
     while($row = mysqli_fetch_array($result))
     {
-        array_push($response,array("item_name"=>$row[0],"quantity"=>$row[1],"price"=>$row[2],"total"=>$row[3],"date"=>''));
+        array_push($response,array("item_name"=>$row[0],"quantity"=>$row[1],"price"=>$row[2],"total"=>$row[3]));
     }
     //echo json_encode(array("server_response"=>$response));
     //echo json_encode($response[0]);
@@ -25,11 +23,9 @@ $items=array('Cabbage', 'Turnip', 'Radish', 'Carrot','leaks','dhall','brinjol',
 $n=sizeof($items);
 $reportList=array();
 $total=0;
-$date="2018-09";
 for($x=0;$x<$n;$x++){
-    array_push($reportList,getReport($items[$x],$date));
+    array_push($reportList,getReport($items[$x]));
     $reportList[$x]['item_name']=$items[$x];
-    $reportList[$x]['date']=$date;
     $total+=$reportList[$x]['total'];
 }
 $reportList[sizeof($reportList)]=array("price"=>"Total Outcome","total"=>$total);
