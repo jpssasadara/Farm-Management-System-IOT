5fb3e0cd0abc2775bfc01fdd530592f10209be01
haseeAmarathunga <haseeamarathunga@gmail.com>
2019-01-03 17:16:00 +0530

    Update CustomerSide

---
+++ Customer_side/farmManagement/RegisterStudent.php
@@ -0,0 +1,30 @@
+<?php
+
+define('DB_HOST', '');
+define('DB_NAME', '');
+define('DB_USER','');
+define('DB_PASSWORD','');
+
+//$con=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " .     mysqli_error());
+//$db=mysqli_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysqli_error());
+$mysqli = new mysqli("localhost", "root", '', "fmsmy");
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+$userName = $_POST['username'];
+$password =  $_POST['password'];
+$query = "INSERT INTO registeredstudent(nic,fname,lname,Mobile,Gender,Address ,Occupation,email ,username,password) VALUES ('$_POST[nic]','$_POST[fname]','$_POST[lname]','$_POST[mobile]','$_POST[gender]','$_POST[address]' ,'$_POST[occupation]','$_POST[email]', '$_POST[username]','$_POST[password]')";
+
+$result = mysqli_query($mysqli, $query);
+
+
+if($result)
+{
+    echo "YOUR REGISTRATION IS COMPLETED...";
+}
+else
+{
+    echo $query;
+}
