c8c110531a0e3c4b4678ec46b929d3f209568b91
haseeAmarathunga <haseeamarathunga@gmail.com>
2019-01-04 09:36:44 +0530

    Update Order_food UI

--- Customer_side/farmManagement/Order_foods.php
+++ Customer_side/farmManagement/Order_foods.php
@@ -268,8 +268,56 @@
 <br/>
 <a href="home.html"><button class="btn btn-default">Back</button></a>
 
+<div class="row">
+
+<?php 
+
+$mysqli = new mysqli("localhost", "root", '', "fmsmy");
+// Check connection
+if ($mysqli->connect_error) {
+die("Connection failed: " . $mysqli->connect_error);
+}
+
+$query = "SELECT * FROM items INNER JOIN tbl_images ON items.Image=tbl_images.id";
+
+if ($result = $mysqli->query($query)) {
+    
+    while($row = $result->fetch_array()){
+    echo "
+    <div class='col-md-4'>
+        <div class='alert alert-success'>
+        <div class='thumbnail'>
+            <img src='../../module/Items/upload/$row[name]' style='width:100%'>
+            <div class='caption'>
+            <p>
+            <li><b>Code :</b> $row[Code]</li>
+            <li><b>Name :</b> $row[Name]</li>
+            <li><b>Price :</b> $row[Price]</li>
+            <li><b>Quantity :</b> $row[Amount]</li>
+            <li><b>Unit :</b> $row[Unit]</li>
+            <li><b>Discount :</b> $row[Discount]</li>
+            <li><b>Type :</b> $row[Type]</li> 
+            </p>
+            <a href='shoppingcart_registration.php?Iname=$row[Name]&Icode=$row[Code]'>
+            <button class='btn btn-success btn-sq-lg'>Add to cart here!</button></a>
+            
+
+            </div>
+        </a>
+        </div>
+        </div>
+    </div>";
+    
+    }
+
+}
+?>
+</div>
 
 
+
+
+    
 <?php
 // Create connection
 
@@ -285,32 +333,31 @@ if ($result = $mysqli->query($query)) {
 
     /* fetch associative array */
 
-    while ($row = $result->fetch_array()) {
-        //echo $row['name'];
-        echo "<div class='well'>";
-        $a="<img src='../../module/Items/upload/$row[name]'/>";
-        echo "<ul class='list-unstyled'>";
+    // while ($row = $result->fetch_array()) {
+    //     //echo $row['name'];
+    //     echo "<div class='well'>";
+    //     $a="<img src='../../module/Items/upload/$row[name]'/>";
+    //     echo "<ul class='list-unstyled'>";
 
-        echo " 
+    //     echo " 
               
-              <div class='row'><div class='col-sm-4'>$a</div>
-              <div class='col-sm-8'>
-              <form action='shoppingcart_registration.php.php' method='get'>
-              <li>$row[Code]</li>
-              </div>
-              <li>$row[Name]</li>
-              <li>$row[Price]</li>
-              <li>$row[Amount]</li>
-              <li>$row[Unit]</li>
-              <li>$row[Discount]</li>
-              <li>$row[Type]</li>
-              <br/>
-              </form>
-               <a href='shoppingcart_registration.php?Iname=$row[Name]&Icode=$row[Code]'>Add to cart here!</a></div>";
+    //           <div class='row'><div class='col-sm-4'>$a</div>
+    //           <div class='col-sm-8'>
+    //           <form action='shoppingcart_registration.php.php' method='get'>
+    //           <li>$row[Code]</li>
+    //           <li>$row[Name]</li>
+    //           <li>$row[Price]</li>
+    //           <li>$row[Amount]</li>
+    //           <li>$row[Unit]</li>
+    //           <li>$row[Discount]</li>
+    //           <li>$row[Type]</li>
+    //           <br/>
+    //           </form>
+    //            <a href='shoppingcart_registration.php?Iname=$row[Name]&Icode=$row[Code]'>Add to cart here!</a></div>";
 
-        echo("</div>");
-    }
-    "</ul>";
+    //     echo("</div>");
+    // }
+    // "</ul>";
     /* free result set */
     $result->free();
 }
@@ -318,7 +365,7 @@ if ($result = $mysqli->query($query)) {
 /* close connection */
 $mysqli->close();
 ?>
-
+</div>
 
 </div>
 
